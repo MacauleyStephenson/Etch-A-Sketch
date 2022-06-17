fc58255a6f17de6bd38e150fa65994c98583cd35
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-06-17 09:16:51 +0100

    implimented basic mouseover effect for Etch-a-sketch

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ function makeRows(rows, cols) {
 };
 
 makeRows(16, 16);
+
+let test = document.getElementById("container");
+
+test.addEventListener("mouseenter", function (event) {
+	event.target.style.color = "purple";
+
+
+	setTimeout(function () {
+		event.target.style.color = "";
+	}, 500);
+}, false);
+
+// This handler will be executed every time the cursor
+// is moved over a different list item
+test.addEventListener("mouseover", function (event) {
+	// highlight the mouseover target
+	event.target.style.color = "orange";
+
+	// reset the color after a short delay
+	setTimeout(function () {
+		event.target.style.color = "";
+	}, 500);
+}, false);
